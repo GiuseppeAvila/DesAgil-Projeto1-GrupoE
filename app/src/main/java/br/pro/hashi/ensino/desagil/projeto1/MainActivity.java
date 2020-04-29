2e146023ccc279c0cd7ffc24046d1f63d9f5a750
Gabriella Cukier <you@example.com>
2020-04-29 17:54:21 -0300

    slides

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -3,14 +3,17 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private MediaPlayer mediaPlayer;
+    float x1, y1, x2, y2;
 
     private TextView textMessage;
     private Button buttonEmergency;
@@ -109,6 +112,28 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+
+    public boolean onTouchEvent(MotionEvent touchEvent){
+        switch(touchEvent.getAction()){
+            case MotionEvent.ACTION_DOWN:
+                 x1 = touchEvent.getX();
+                 y1 = touchEvent.getY();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = touchEvent.getX();
+                y2 = touchEvent.getY();
+                if(x1 < x2){
+                Intent i = new Intent(MainActivity.this, Contatos.class);
+                startActivity(i);
+            }else if(x1 > x2){
+                Intent i = new Intent(MainActivity.this, Morse.class);
+                startActivity(i);
+            }
+            break;
+        }
+        return false;
     }
 
 }
