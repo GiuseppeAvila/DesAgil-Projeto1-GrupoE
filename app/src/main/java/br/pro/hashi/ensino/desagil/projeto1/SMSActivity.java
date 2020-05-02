386c6aec0d8be2e7db342790f8dfa086d04188be
Gabriella Cukier <you@example.com>
2020-05-02 09:09:02 -0300

    sms funcionando

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/SMSActivity.java
@@ -1,8 +1,13 @@
 package br.pro.hashi.ensino.desagil.projeto1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.telephony.PhoneNumberUtils;
 import android.telephony.SmsManager;
 import android.view.MotionEvent;
@@ -24,26 +29,37 @@ import android.widget.Toast;
 public class SMSActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
         float x1, y1, x2, y2;
         private ImageButton contacts;
+        private Button buttonPermission;
+        private Button buttonSend;
+
+    private static final int REQUEST_SEND_SMS = 0;
+
 
     // Método de conveniência para mostrar uma bolha de texto.
     private void showToast(String text) {
-
         // Constrói uma bolha de duração curta.
         Toast toast = Toast.makeText(this, text, Toast.LENGTH_SHORT);
-
         // Mostra essa bolha.
         toast.show();
     }
+    // Método de conveniência para iniciar a SMSActivity.
+    private void startSMSActivity() {
+
+        // Constrói uma Intent que corresponde ao pedido de "iniciar Activity".
+        Intent intent = new Intent(this, SMSActivity.class);
+
+        // Inicia a Activity especificada na Intent.
+        startActivity(intent);
+    }
 
 
-        @Override
+
+    @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_sms);
 
             contacts = (ImageButton) findViewById(R.id.procurar_contatos);
-
-
             contacts.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -52,9 +68,41 @@ public class SMSActivity extends AppCompatActivity implements AdapterView.OnItem
             });
 
 
+        buttonPermission = (Button) findViewById(R.id.buttonPermission);
+        buttonPermission.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonPermission.setVisibility(View.GONE);
+            }
+        });
+
+            buttonPermission.setOnClickListener((view) -> {
 
 
-            Spinner spinner_contacts = (Spinner) findViewById(R.id.spinner);
+                    // Verifica se o aplicativo tem a permissão desejada.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.SEND_SMS) == PackageManager.PERMISSION_GRANTED) {
+
+                        // Se tem, podemos iniciar a SMSActivity direto.
+                        startSMSActivity();
+                    } else {
+
+                        // Senão, precisamos pedir essa permissão.
+
+                        // Cria um vetor de permissões a pedir. Como queremos
+                        // uma só, parece um pouco feio, mas é bem conveniente
+                        // quando queremos pedir várias permissões de uma vez.
+                        String[] permissions = new String[]{
+                                Manifest.permission.SEND_SMS,
+                        };
+
+                        ActivityCompat.requestPermissions(this, permissions, REQUEST_SEND_SMS);
+                    }
+                });
+
+
+
+
+                Spinner spinner_contacts = (Spinner) findViewById(R.id.spinner);
             ArrayAdapter<CharSequence> adapter_contacts = ArrayAdapter.createFromResource(this,R.array.contatos, android.R.layout.simple_spinner_item);
 // Specify the layout to use when the list of choices appears
             adapter_contacts.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -101,7 +149,9 @@ public class SMSActivity extends AppCompatActivity implements AdapterView.OnItem
                // textMessage.setselection(0);
             });
 
-        }
+
+
+            }
 
 
         public boolean onTouchEvent(MotionEvent touchEvent){
@@ -143,4 +193,24 @@ public class SMSActivity extends AppCompatActivity implements AdapterView.OnItem
         Intent intent = new Intent(this, ListaContatos.class);
         startActivity(intent);
     }
+
+
+    // Como consequência da chamada de requestPermissions acima, este
+    // método é chamado quando o usuário responde o pedido de permissão.
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+
+        // Verifica se de fato é uma resposta ao pedido acima e se a
+        // resposta foi positiva. As respostas estão armazenadas no
+        // vetor grantResults, que pode estar vazio se o usuário
+        // escolheu simplesmente ignorar o pedido e não responder nada.
+        if (requestCode == REQUEST_SEND_SMS && grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+            // Se foi positiva, podemos iniciar a SMSActivity.
+            startSMSActivity();
+        }
+    }
 }
+
+
+
