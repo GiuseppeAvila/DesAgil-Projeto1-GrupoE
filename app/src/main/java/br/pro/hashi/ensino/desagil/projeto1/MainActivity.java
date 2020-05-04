ac4b422c0b55cd9f9f1bf187ade55e7b3b043f1e
carolinechaim <caroline.chaim@gmail.com>
2020-05-04 16:31:13 -0300

    So falta adicionar os contatos e os drop down

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/MainActivity.java
@@ -15,112 +15,116 @@ import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+
+//CHAMANDO AS VARIAVEIS DA ATIVIDADE
     private MediaPlayer mediaPlayer;
     float x1, y1, x2, y2;
 
-    private TextView textMessage;
-    private Button buttonEmergency;
-    private Button buttonHunger;
-    private Button buttonThirst;
-    private Button buttonPain;
-    private Button buttonToilet;
-
+// INÍCIO DA ATIVIDADE
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+//CHAMANDO OS BOTTÕES DA ATIVIDADE
+        Button buttonEmergency =findViewById(R.id.buttonEmergencia);
+        Button buttonHunger = findViewById(R.id.buttonFome);
+        Button buttonThirst = findViewById(R.id.buttonSede);
+        Button buttonPain = findViewById(R.id.buttonDor);
+        Button buttonToilet = findViewById(R.id.buttonBanheiro);
 
-        buttonEmergency = (Button) findViewById(R.id.buttonEmergencia);
-        textMessage = (TextView) findViewById(R.id.textMensagem);
+
+//CHAMANDO O TEXTO DA ATIVIDADE
+        TextView textMessage = findViewById(R.id.textMensagem);
+
+
+
+//CHAMANDO O PLAYER DA ATIVIDADE
         mediaPlayer = MediaPlayer.create(this, R.raw.emergency);
-        buttonEmergency.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if (textMessage.getText().toString().equals("! EMERGÊNCIA !")) {
-                    // Pause the music player
-                    mediaPlayer.pause();
-                    textMessage.setVisibility(View.INVISIBLE);
-                    textMessage.setText("");
-                    // If it's not playing
-                } else {
-                    // Resume the music player
-                    mediaPlayer.start();
-                    textMessage.setText("! EMERGÊNCIA !");
-                    textMessage.setBackgroundColor(0xFFCC0000);
-                    textMessage.setVisibility(View.VISIBLE);
-                }
+
+
+
+//FUNÇÃO DO BOTÃO EMERGENCY
+        buttonEmergency.setOnClickListener((view) -> {
+            if (textMessage.getText().toString().equals("! EMERGÊNCIA !")) {
+                // Pause the music player
+                mediaPlayer.pause();
+                textMessage.setVisibility(View.INVISIBLE);
+                textMessage.setText("");
+                // If it's not playing
+            } else {
+                // Resume the music player
+                mediaPlayer.start();
+                textMessage.setText("! EMERGÊNCIA !");
+                textMessage.setBackgroundColor(0xFFCC0000);
+                textMessage.setVisibility(View.VISIBLE);
             }
         });
 
-        buttonHunger = (Button) findViewById(R.id.buttonFome);
-        buttonThirst = (Button) findViewById(R.id.buttonSede);
-        buttonPain = (Button) findViewById(R.id.buttonDor);
-        buttonToilet = (Button) findViewById(R.id.buttonBanheiro);
+//FUNÇAO DO BOTÃO HUNGER
 
-        buttonHunger.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
-                if (textMessage.getText().toString().equals("Estou com fome!")) {
-                    textMessage.setVisibility(View.INVISIBLE);
-                    textMessage.setText("");
-                } else {
-                    textMessage.setText("Estou com fome!");
-                    textMessage.setBackgroundColor(0xFFFFFFFF);
-                    textMessage.setVisibility(View.VISIBLE);
-                }
+        buttonHunger.setOnClickListener((view) -> {
+            if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
+            if (textMessage.getText().toString().equals("Estou com fome!")) {
+                textMessage.setVisibility(View.INVISIBLE);
+                textMessage.setText("");
+            } else {
+                textMessage.setText("Estou com fome!");
+                textMessage.setBackgroundColor(0xFFFFFFFF);
+                textMessage.setVisibility(View.VISIBLE);
             }
         });
 
 
-        buttonThirst.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
-                if (textMessage.getText().toString().equals("Estou com sede!")) {
-                    textMessage.setVisibility(View.INVISIBLE);
-                    textMessage.setText("");
-                } else {
-                    textMessage.setText("Estou com sede!");
-                    textMessage.setBackgroundColor(0xFFFFFFFF);
-                    textMessage.setVisibility(View.VISIBLE);
-                }
+//FUNÇÃO DO BOTÃO THIRST
+        buttonThirst.setOnClickListener((view) -> {
+            if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
+            if (textMessage.getText().toString().equals("Estou com sede!")) {
+                textMessage.setVisibility(View.INVISIBLE);
+                textMessage.setText("");
+            } else {
+                textMessage.setText("Estou com sede!");
+                textMessage.setBackgroundColor(0xFFFFFFFF);
+                textMessage.setVisibility(View.VISIBLE);
             }
         });
 
-        buttonPain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
-                if (textMessage.getText().toString().equals("Estou com dor!")) {
-                    textMessage.setVisibility(View.INVISIBLE);
-                    textMessage.setText("");
-                } else {
-                    textMessage.setText("Estou com dor!");
-                    textMessage.setBackgroundColor(0xFFFFFFFF);
-                    textMessage.setVisibility(View.VISIBLE);
-                }
+
+//FUNÇÃO DO BOTÃO PAIN
+        buttonPain.setOnClickListener((view) -> {
+            if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
+            if (textMessage.getText().toString().equals("Estou com dor!")) {
+                textMessage.setVisibility(View.INVISIBLE);
+                textMessage.setText("");
+            } else {
+                textMessage.setText("Estou com dor!");
+                textMessage.setBackgroundColor(0xFFFFFFFF);
+                textMessage.setVisibility(View.VISIBLE);
             }
         });
 
-        buttonToilet.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
-                if (textMessage.getText().toString().equals("Preciso ir ao banheiro!")) {
-                    textMessage.setVisibility(View.INVISIBLE);
-                    textMessage.setText("");
-                } else {
-                    textMessage.setText("Preciso ir ao banheiro!");
-                    textMessage.setBackgroundColor(0xFFFFFFFF);
-                    textMessage.setVisibility(View.VISIBLE);
-                }
+
+//FUNÇÃO DO BOTÃO TOILET
+        buttonToilet.setOnClickListener((view) -> {
+            if (mediaPlayer.isPlaying()) { mediaPlayer.pause(); }
+            if (textMessage.getText().toString().equals("Preciso ir ao banheiro!")) {
+                textMessage.setVisibility(View.INVISIBLE);
+                textMessage.setText("");
+            } else {
+                textMessage.setText("Preciso ir ao banheiro!");
+                textMessage.setBackgroundColor(0xFFFFFFFF);
+                textMessage.setVisibility(View.VISIBLE);
             }
         });
+
     }
 
 
+
+//MÉTODO PARA SLIDE DE TELA
+    // SMSACTIVITY
+    // MORSE
     public boolean onTouchEvent(MotionEvent touchEvent){
         switch(touchEvent.getAction()){
             case MotionEvent.ACTION_DOWN:
@@ -133,10 +137,11 @@ public class MainActivity extends AppCompatActivity {
                 if(x1 < x2){
                 Intent i = new Intent(MainActivity.this, SMSActivity.class);
                 startActivity(i);
-            }else if(x1 > x2){
-                Intent i = new Intent(MainActivity.this, Morse.class);
-                startActivity(i);
             }
+                //else if(x1 > x2){
+                //Intent i = new Intent(MainActivity.this, Morse.class);
+                //startActivity(i);
+            //}
             break;
         }
         return false;
