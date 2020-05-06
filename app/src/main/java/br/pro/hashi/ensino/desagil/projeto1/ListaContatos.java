62e50754b1bb5aea72aab4a84063e1972f7e781e
Gabriella Cukier <you@example.com>
2020-05-06 11:06:34 -0300

    lista contatos

--- app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ListaContatos.java
+++ app/src/main/java/br/pro/hashi/ensino/desagil/projeto1/ListaContatos.java
@@ -3,12 +3,32 @@ package br.pro.hashi.ensino.desagil.projeto1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ListaContatos extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_contatos);
+
+        String[] phoneNumber = getResources().getStringArray(R.array.numeros);
+        String[] textPhone = getResources().getStringArray(R.array.nomes);
+        ListView phones =findViewById(R.id.textNumeros);
+        ListView contacts = findViewById(R.id.textViewNomes);
+
+        Map<String, String> map = new HashMap<>();
+
+
     }
+
 }
